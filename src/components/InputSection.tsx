380bd16090dc515b43faea62712988610ce63ad5
bendik <bendiku95@gmail.com>
2024-01-13 22:28:54 +0100

    more style

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -6,8 +6,8 @@ import './InputSection.scss'
 const InputSection = () => {
 
   const [prefix, setPrefix] = useState<string>('');
-  const [suffix, setSuffix] = useState<string>('');
   const [infix, setInfix] = useState<string>('');
+  const [suffix, setSuffix] = useState<string>('');
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -16,36 +16,30 @@ const InputSection = () => {
     } else if (name === 'suffix') {
       setSuffix(value);
     }
+
+    if (prefix !== '' && suffix !== '') {
+      setInfix(value);
+    }
   };
 
   return (
     <>
       <div className="container">
-
         <div className="demo-flex-spacer"></div>
-
         <div className="webflow-style-input">
-          <input className="" type="text"></input>
-          <button type="submit"><i className="icon ion-android-arrow-forward"></i></button>
+          <input className="" type="text" name="prefix" value={prefix} onChange={handleInputChange} />
         </div>
-
         <div className="demo-flex-spacer"></div>
-
       </div>
       <div className="form__group field">
-        <input type="input" className="form__field" placeholder='' name="name" id='name' readOnly required tabIndex={-1} value={infix} />
+        <input type="input" className="form__field" placeholder='' name="name" id='name' readOnly tabIndex={-1} value={infix} />
       </div>
       <div className="container">
-
         <div className="demo-flex-spacer"></div>
-
         <div className="webflow-style-input">
-          <input className="" type="text"></input>
-          <button type="submit"><i className="icon ion-android-arrow-forward"></i></button>
+        <input className="" type="text" name="suffix" value={suffix} onChange={handleInputChange} />
         </div>
-
         <div className="demo-flex-spacer"></div>
-
       </div>
     </>
   );
